bd52706c602d1301ef0dbf2140bee3793c907ea2
Viturna <148191882+Viturna@users.noreply.github.com>
2026-08-20 20:59:42 +0200

    api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // On utilise l'URL de production Coolify directement comme demandé.
-// const API_URL = 'https://archiv.api.omniscientproject.com/api';
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://archiv.api.omniscientproject.com/api';
+// const API_URL = 'http://localhost:3000/api';
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true, // Très important pour envoyer/recevoir les cookies (JWT)
