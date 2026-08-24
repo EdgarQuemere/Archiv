b189cd5fa2856db488b82901301e776e27264172
Viturna <148191882+Viturna@users.noreply.github.com>
2026-08-24 23:40:26 +0200

    maj url api

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,6 +1,6 @@
 export const getFileUrl = (url) => {
     if (!url) return '';
-    if (url.startsWith('/api/files/')) {
+    if (url.startsWith('/files/')) {
         const backendUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000';
         return `${backendUrl}${url}`;
     }
